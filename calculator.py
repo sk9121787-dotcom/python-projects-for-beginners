af75ebd1a770c186cc57fdfc0ce8d91390824984
sk9121787-dotcom <sk9121787@gmail.com>
2026-07-01 11:56:00 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+num1 = float(input("Pehla number daal: "))
+num2 = float(input("Dusra number daal: "))
+
+print("Jod:", num1 + num2)
+print("Ghatav:",num1 - num2)
+print("Guna:", num1*num2)
+print("Bhaag:", num1 / num2)
