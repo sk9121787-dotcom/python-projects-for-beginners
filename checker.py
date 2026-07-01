38cf3a29e1d934a0e308d17e1548292a6dd52371
sk9121787-dotcom <sk9121787@gmail.com>
2026-07-01 12:10:14 +0530

    Create checker.py

---
+++ checker.py
@@ -0,0 +1,12 @@
+marks = int(input("Number daal 0-100 ke beech: "))
+
+if marks >= 90:
+  print("Grade A - Bohot badhiya!")
+elif marks >= 75:
+  print("Grade B - Achha hai")
+elif marks >= 60:
+  print("Grade C - Theek hai")
+elif marks >= 33:
+  print("Grade D - Pass ho gaya")
+else:
+  print("Grade F - Fail. Mehnat kar")
